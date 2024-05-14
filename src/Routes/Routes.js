1657a374b2cd737ed4b625415ea82d013a3fdec9
pankaj <SFS.pankaj2022@gmail.com>
2024-05-14 14:11:06 +0530

    new changes

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,12 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import Homes from "../pages/Home"
 import Cars from "../pages/Cars";
-import Convert from "../pages/Convertcsv";
-import ExcelSheetReader from "../pages/Papaparse";
-import FortuneSheet from "../pages/Fortunesheet";
 
-import CsvDatafile from "../pages/CsvData";
-import Draganddrop from "../pages/DragandDrop";
+
+
+
+
 import Instagramdownload from "../pages/Instagramdownload";
 
 const PageRoutes=()=>{
@@ -14,11 +13,10 @@ const PageRoutes=()=>{
         <Routes>
             <Route path="/" element={<Instagramdownload/>} />
             <Route path ="/cars" element={<Cars/>} />
-            <Route path ="/convert" element={<Convert/>} />
-            <Route path ="/json" element={< ExcelSheetReader/>} />
-            <Route path ="/fortune-sheets" element={<FortuneSheet/>}/>
-            <Route path ="/csv" element={<CsvDatafile/> }/>
-            <Route path ="/draganddrop" element={<Draganddrop/>} />
+           
+           
+       
+    
             <Route path ="/download" element={<Instagramdownload/>}/>
 
         </Routes>
